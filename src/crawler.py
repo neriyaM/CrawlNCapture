bdbae91c7925385c16f20b99da8d0aa4a0f8127c
EC2 Default User <ec2-user@ip-172-31-7-191.us-east-2.compute.internal>
2021-08-23 16:52:07 +0000

    Add test ec2

--- src/crawler.py
+++ src/crawler.py
@@ -5,7 +5,13 @@ import time
 
 def crawl(address, load_time, chrome_path, driver_path):
     options = Options()
-    options.binary_location = chrome_path
+    options.add_argument("--headless")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    #options.binary_location = chrome_path
     driver = webdriver.Chrome(chrome_options=options, executable_path=driver_path)
     driver.delete_all_cookies()
     driver.get(address)
