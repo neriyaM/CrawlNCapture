ba8971e40a8282003acdee92713950917e2012d1
Neriya Mazzuz <neriya92@gmail.com>
2022-02-11 13:34:57 +0200

    Alexa

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.utils import build_filename
 import argparse
 import json
 from src.consts import ConfigKeys
+import sqlite3 as sql
 
 
 def main():
@@ -11,15 +12,21 @@ def main():
     args = parser.parse_args()
 
     config = load_config(args.config)
-    for i in range(3):
-        for url in config[ConfigKeys.URLS]:
-            print("Start {}".format(url))
-            filename = build_filename(url)
+    domains = load_domains(config[ConfigKeys.DOMAINS_DB])
+    #domains = load_top_domains()
+    with open('domains.txt', 'w') as f:
+        for domain in domains:
+            f.write(domain + '\n')
+    exit()
+    for i in range(1):
+        for domain in domains:
+            print("Start {}".format(domain))
+            filename = build_filename(domain)
             sniffer = Sniffer(filename, config[ConfigKeys.SNIFF_FILTER])
             sniffer.start()
-            crawler.crawl(url, config[ConfigKeys.CRAWL_TIME])
+            crawler.crawl(domain, config[ConfigKeys.CRAWL_TIME])
             sniffer.stop()
-            print("Stop {}".format(url))
+            print("Stop {}".format(domain))
 
 
 def create_arg_parser():
@@ -31,6 +38,22 @@ def create_arg_parser():
 def load_config(path: str):
     with open(path, 'r') as f:
         return json.load(f)
+
+
+def load_domains(path):
+    conn = sql.connect(path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT DISTINCT(domain) from DOMAINS")
+    domains = cursor.fetchall()
+    return [domain[0] for domain in domains]
+
+
+def load_top_domains():
+    with open('top-1m.csv', 'r') as f:
+        content = f.read()
+
+    domains = [x.split(',')[-1] for x in content.splitlines()]
+    return domains[:1000]
 
 
 if __name__ == "__main__":
--- src/consts.py
+++ src/consts.py
@@ -1,6 +1,4 @@
 class ConfigKeys:
-    URLS = "URLS"
     CRAWL_TIME = "CrawlTime"
-    CHROME_PATH = "ChromePath"
-    DRIVER_PATH = "DriverPath"
     SNIFF_FILTER = "SniffFilter"
+    DOMAINS_DB = "DomainsDB"
